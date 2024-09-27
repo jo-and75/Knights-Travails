cf99a0b787a25b45ec452c90d36ff7b06f04c040
jo-and75 <jaotika@gmail.com>
2024-09-27 12:33:30 +0300

    Make instance variables in initialize methods accessible

--- lib/knights.rb
+++ lib/knights.rb
@@ -1,10 +1,26 @@
 class Knights 
+  def initialize 
+    @start_pos = nil 
+    @finish_pos = nil
+  end 
+
+  def knight_moves(start,finish) 
+    @start_pos = Node.new(start)
+    puts @start_pos.value
+
+  end
 
 
 
-  class Node
-    def initialize(value, next_node = []) 
-      @value = value 
+
+
+
+
+
+  class Node 
+    attr_accessor :value , :next_node
+    def initialize(value, next_node = [])
+      @value = value
       @next_node = next_node
     end
   end
---
+++ main.rb
@@ -0,0 +1,4 @@
+require_relative 'lib/knights'
+
+play = Knights.new 
+play.knight_moves([0,0],[3,3])
