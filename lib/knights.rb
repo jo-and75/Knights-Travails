24d235c77181c1f011e4d182fe08d30fed92e103
jo-and75 <jaotika@gmail.com>
2024-10-01 10:52:53 +0300

    Apply rubocop changes

--- lib/knights.rb
+++ lib/knights.rb
@@ -1,27 +1,26 @@
 class Knights
-  def initialize(start,finish) 
+  def initialize(start, finish)
     @finish_pos = finish
-    @start_pos = knight_moves(start,@finish_pos)
+    @start_pos = knight_moves(start, @finish_pos)
   end
 
   MOVES = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]
 
-  def knight_moves(start, finish, queue = [])    
-    queue.push(start) unless queue.length > 1 #ensures start is only pushed on the first recursive call.
-    root_node = Node.new(queue[0])  
-    return nil if finish[0] < 0 || finish[0] > 7 || finish[1] < 0 || finish[1] > 7 
-    return nil if root_node.nil? 
+  def knight_moves(start, finish, queue = [])
+    queue.push(start) unless queue.length > 1 # ensures start is only pushed on the first recursive call.
+    root_node = Node.new(queue[0])
+    return nil if (finish[0]).negative? || finish[0] > 7 || (finish[1]).negative? || finish[1] > 7
+    return nil if root_node.nil?
     return root_node if root_node.value == finish
-  
+
     MOVES.each do |arr|
       result = arr.zip(queue[0]).map { |a, b| a + b }
-      root_node.next_node.push(result) if (0 <= result[0] && result[0] <= 7) && (0 <= result[1] && result[1] <= 7)
-    end    
-    queue.shift   
-    root_node.next_node.each {|node| queue << node}  
+      root_node.next_node.push(result) if (result[0] >= 0 && result[0] <= 7) && (result[1] >= 0 && result[1] <= 7)
+    end
+    queue.shift
+    root_node.next_node.each { |node| queue << node }
     p root_node
-    knight_moves(root_node.next_node,finish,queue)  
-   
+    knight_moves(root_node.next_node, finish, queue)
   end
 
   class Node
