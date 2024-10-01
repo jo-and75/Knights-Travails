c7f84d1168bdcf38a9341a432a4436986b59a8c2
jo-and75 <jaotika@gmail.com>
2024-10-01 11:33:33 +0300

    Change base case of recursive call

--- lib/knights.rb
+++ lib/knights.rb
@@ -1,26 +1,37 @@
+# frozen_string_literal: true
+
+require 'set'
+
 class Knights
   def initialize(start, finish)
     @finish_pos = finish
     @start_pos = knight_moves(start, @finish_pos)
   end
 
-  MOVES = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]
+  MOVES = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]].freeze
 
-  def knight_moves(start, finish, queue = [])
+  def knight_moves(start, finish, queue = [], visited_pos = Set.new([]))
     queue.push(start) unless queue.length > 1 # ensures start is only pushed on the first recursive call.
+    p queue
     root_node = Node.new(queue[0])
-    return nil if (finish[0]).negative? || finish[0] > 7 || (finish[1]).negative? || finish[1] > 7
-    return nil if root_node.nil?
-    return root_node if root_node.value == finish
+    return  root_node if queue[0].empty?
+    # return  if (finish[0]).negative? || finish[0] < 7 || (finish[1]).negative? || finish[1] > 7
+    return  if root_node.nil?
 
     MOVES.each do |arr|
       result = arr.zip(queue[0]).map { |a, b| a + b }
-      root_node.next_node.push(result) if (result[0] >= 0 && result[0] <= 7) && (result[1] >= 0 && result[1] <= 7)
+      if (result[0] >= 0 && result[0] <= 7) && (result[1] >= 0 && result[1] <= 7) && visited_pos.include?(result) == false
+        root_node.next_node.push(result)
+      end
     end
-    queue.shift
+    if visited_pos.include?(queue[0]) == false
+      visited_pos << queue.shift
+    else
+      queue.shift
+    end
+    # p visited_pos
     root_node.next_node.each { |node| queue << node }
-    p root_node
-    knight_moves(root_node.next_node, finish, queue)
+    knight_moves(root_node.next_node, finish, queue, visited_pos)
   end
 
   class Node
